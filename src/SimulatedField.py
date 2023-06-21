c602763cdb17540a42d19736191dfcdf3186cff9
Jeffrey Ke <jeffke613@gmail.com>
2023-06-21 09:38:54 -0700

    close #9
    Separated createHeatmap into its own file.

--- src/SimulatedField.py
+++ src/SimulatedField.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import floor
 from src.SensorReadingsStamped import SensorReadingsStamped
+from src.HeatMap import createHeatmap
 
 class SimulatedField:
     
@@ -17,7 +18,7 @@ class SimulatedField:
 
         plt.ion()
 
-        self._field = self.create_heatmap(height, width, feature_coords, feature_sigma)
+        self._field = createHeatmap(height, width, feature_coords, feature_sigma)
         self._fig, self._ax = plt.subplots()
 
         im = self._ax.imshow(self._field, 
@@ -95,28 +96,7 @@ class SimulatedField:
         return self._robot_state
 
 
-    def create_heatmap(self, rows, cols, hotspot_coords, sigma=10):
-        """
-        Create a 2D heatmap with a hotspot at a given row and column.
-
-        :param rows: Number of rows in the heatmap
-        :param cols: Number of columns in the heatmap
-        :param hotspot_row: The row index for the hotspot
-        :param hotspot_col: The column index for the hotspot
-        :param sigma: The standard deviation of the Gaussian, controlling the "spread" of the hotspot
-        """
-        # Create a grid of coordinates
-        y = np.arange(rows)
-        x = np.arange(cols)
-        X, Y = np.meshgrid(x, y)
-        hotspot_col = hotspot_coords[1]
-        hotspot_row = hotspot_coords[0]
-
-
-        # Create the Gaussian function centered at the hotspot
-        heatmap = np.exp(-((X - hotspot_col)**2 + (Y - hotspot_row)**2) / (2 * sigma**2))
-
-        return heatmap
+    
 
 
     
