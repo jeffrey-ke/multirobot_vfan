81cda73823ffe0dba5bbb3280cc5df94cd01cd4c
Jeffrey Ke <jeffke613@gmail.com>
2023-07-04 10:23:36 -0700

    Removed math.floor and SensorReadingsStamped
    Removed obsolete libraries.

--- src/SimulatedField/SimulatedField.py
+++ src/SimulatedField/SimulatedField.py
@@ -1,7 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from math import floor
-from src.SensorReadingsStamped import SensorReadingsStamped
 from src.HeatMap import createHeatmap
 
 class SimulatedField:
