505d0d5848595a7b8bf10d8980073a191de6b255
Jeffrey Ke <jeffke613@gmail.com>
2023-07-04 10:25:24 -0700

    Commented out old code

--- src/SimulatedField/SimulatedField.py
+++ src/SimulatedField/SimulatedField.py
@@ -101,18 +101,18 @@ class SimulatedField:
         plt.pause(0.1)
         plt.show()
 
-    def runSim(self, steps=100):
-        for _ in range(steps):
-            robot_poses = [self._robot_state._data[robot]["coords"] for robot in self._robot_state._data.keys()]
+    # def runSim(self, steps=100):
+    #     for _ in range(steps):
+    #         robot_poses = [self._robot_state._data[robot]["coords"] for robot in self._robot_state._data.keys()]
 
-            gradient_xy = np.array(
-                [floor(component + 1 if component > 0 else component - 1) for component in self.getGradient()[:2]]
-                )
+    #         gradient_xy = np.array(
+    #             [floor(component + 1 if component > 0 else component - 1) for component in self.getGradient()[:2]]
+    #             )
 
-            robot_poses = (robot_poses + gradient_xy).astype(int)
+    #         robot_poses = (robot_poses + gradient_xy).astype(int)
 
-            self.updateField(robot_ids=list(self._robot_state._data.keys()),
-                             poses=robot_poses)
+    #         self.updateField(robot_ids=list(self._robot_state._data.keys()),
+    #                          poses=robot_poses)
         
 
     # def getGradient(self):
@@ -129,41 +129,41 @@ class SimulatedField:
         
     #     return -np.cross(R_01, R_02)
 
-    def updateField(self, robot_ids, poses):
-        """
-        Places the robots on the heatmap.
-        """
-        if len(robot_ids) < 1:
-            raise Exception("At least one robot must be added to the simulation.")
-        if len(robot_ids) != len(poses):
-            raise Exception("Amount of poses provided does not match number of robots provided.")
+    # def updateField(self, robot_ids, poses):
+    #     """
+    #     Places the robots on the heatmap.
+    #     """
+    #     if len(robot_ids) < 1:
+    #         raise Exception("At least one robot must be added to the simulation.")
+    #     if len(robot_ids) != len(poses):
+    #         raise Exception("Amount of poses provided does not match number of robots provided.")
         
-        self._ax.clear()
+    #     self._ax.clear()
         
-        sensor_readings = [self._field[x][y] for x, y in poses]
-        self._robot_state = SensorReadingsStamped(robot_ids=robot_ids, poses=poses, sensor_readings=sensor_readings)
+    #     sensor_readings = [self._field[x][y] for x, y in poses]
+    #     self._robot_state = SensorReadingsStamped(robot_ids=robot_ids, poses=poses, sensor_readings=sensor_readings)
 
-        for robot in robot_ids:
-            self._ax.annotate(robot, 
-                              (self._robot_state._data[robot]["coords"][0], 
-                               self._robot_state._data[robot]["coords"][1]),#(x,y) coord
-                              color='b') 
+    #     for robot in robot_ids:
+    #         self._ax.annotate(robot, 
+    #                           (self._robot_state._data[robot]["coords"][0], 
+    #                            self._robot_state._data[robot]["coords"][1]),#(x,y) coord
+    #                           color='b') 
             
-            self._ax.scatter(self._robot_state._data[robot]["coords"][0], 
-                             self._robot_state._data[robot]["coords"][1],
-                             color="b")
+    #         self._ax.scatter(self._robot_state._data[robot]["coords"][0], 
+    #                          self._robot_state._data[robot]["coords"][1],
+    #                          color="b")
         
-        self._ax.imshow(self._field, 
-                       cmap='hot', 
-                       interpolation='nearest',
-                       origin="lower", 
-                       aspect="auto")
+    #     self._ax.imshow(self._field, 
+    #                    cmap='hot', 
+    #                    interpolation='nearest',
+    #                    origin="lower", 
+    #                    aspect="auto")
         
-        self._fig.canvas.draw()
-        plt.pause(0.1)
-        plt.show()
+    #     self._fig.canvas.draw()
+    #     plt.pause(0.1)
+    #     plt.show()
 
-        return self._robot_state
+    #     return self._robot_state
 
 
     
