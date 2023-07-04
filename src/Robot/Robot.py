8c020a1712698bed943275d18a26a81e11994634
Jeffrey Ke <jeffke613@gmail.com>
2023-07-04 10:22:39 -0700

    Update Robot.py
    Created Robot.py.

--- src/Robot/Robot.py
+++ src/Robot/Robot.py
@@ -1,15 +1,28 @@
+import math
 class Robot:
-    _velocity: list = [0.0, 0.0]
-    _position: list = [0.0, 0.0]
-    _heading: float = 0.0
-    _sensor_reading: float = 0.0
-    def __init__(self) -> None:
-        pass
 
-    def command(self, velocity=[0.0, 0.0]):
+    _id = ""
+    _pose = [0.0, 0.0]
+    _speed = 5
+    _sensor_reading: float = 0.0
+    _is_leader = False
+
+    def __init__(self, id, location, leader=False):
+        self._id = id
+        self._pose = list(location)
+        self._is_leader = leader
+
+    def readSensor(self, reading: float):
+        self._sensor_reading = reading
+
+    def giveCommand(self, R_dot: list):
+        """_summary_
+
+        Args:
+            R_dot (list): R_dot = [x_dot, y_dot]
         """
-            Cluster Space Controller calls this function on all of
-            the robots in its cluster and gives it a v
-        """
-        self._velocity = velocity
+        theta = math.atan2(R_dot[1], R_dot[0])
+        x,y = self._speed * math.cos(theta), self._speed * math.sin(theta)
+        self._pose[0], self._pose[1] = self._pose[0] + x, self._pose[1] + y
+
         
