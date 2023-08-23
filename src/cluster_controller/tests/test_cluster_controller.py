5293983d7830a998d15c394eabc465523ae66604
Jeffrey Ke <jeffrey@bearflagrobotics.com>
2023-08-22 22:00:55 -0700

    got 1st unit testing working

--- src/cluster_controller/tests/test_cluster_controller.py
+++ src/cluster_controller/tests/test_cluster_controller.py
@@ -1,9 +1,8 @@
-import sys
-sys.path.append("/home/jeffrey/repo/ros_ws/src/cluster_controller/cluster_controller")
 
-from cluster_controller.cluster_controller import ClusterController
+from cluster_controller.ClusterController import ClusterController
+import numpy as np
 def test_inv_jacob():
     c = ClusterController()
     c.DeclareRobots(["a", "b", "c"], [[0, 0, 0], [5, 0, 0], [2.5, 5, 0]])
-    assert c.CalculateRDot([0, 0, 0, 0, 0, 0, 0, 0, 0]) \
-                                == [0, 0, 0, 0, 0, 0, 0, 0 , 0]
+    for elem in np.equal(c.CalculateRDot([0, 0, 0, 0, 0, 0, 0, 0, 0]), [0, 0, 0, 0, 0, 0, 0, 0 , 0]):
+        assert elem
