055b89253a4f9009e59e0efdd4a8ebe384af5961
Jeffrey Ke <jeffrey@bearflagrobotics.com>
2023-08-27 22:48:44 -0700

    test for false singular matrix

--- src/cluster_controller/tests/test_cluster_controller.py
+++ src/cluster_controller/tests/test_cluster_controller.py
@@ -7,4 +7,12 @@ def test_inv_jacob():
     np.set_printoptions(suppress=True)
     for elem in np.equal(c.CalculateRDot([0, 0, 0, 0, 0, 0, 0, 0, 0]), [0, 0, 0, 0, 0, 0, 0, 0 , 0]):
         assert elem
-    assert(False)
+
+def test_inv_jacob2():
+    c = ClusterController()
+    c.DeclareRobots(["a", "b", "c"], [[0, 0, 0], [5, 0, 0], [2.5, 5, 0]])
+    np.set_printoptions(suppress=True)
+    r_dot = c.CalculateRDot([10.0, 10.0, 1.0, 0, 0, 0, 0, 0, 0])
+    print(r_dot)
+    for elem in np.equal(r_dot, [0, 0, 0, 0, 0, 0, 0, 0 , 0]):
+        assert not elem
